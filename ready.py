d82f406756a99fd279d333182af086fc4b75f57b
afiqnexea <afiqnrzm@hotmail.com>
2023-08-13 22:59:50 +0800

    ta

--- ready.py
+++ ready.py
@@ -77,9 +77,9 @@ def main():
         sheet.update_acell(f"J{i + 2}", f"{gp}")
 
 
-# schedule.every(10).minutes.do(main)
+schedule.every(1).minutes.do(main)
 
-schedule.every().day.at("22:58").do(main)
+# schedule.every().day.at("22:58").do(main)
 
 while True:
     schedule.run_pending()
