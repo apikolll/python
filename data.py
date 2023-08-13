15b0dd17e7e8100c64e3034ba6806200d9eba7fa
afiqnexea <afiqnrzm@hotmail.com>
2023-08-13 21:17:56 +0800

    Update data.py

--- data.py
+++ data.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService 
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
-WINDOW_SIZE = "1920,1080"
-chrome_options = Options()
-chrome_options.add_argument("--headless:new")
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-                          chrome_options=chrome_options
-                         )
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+
+driver = webdriver.Chrome(options=options)
+
 driver.get("https://www.google.com")
 print(driver.title)
 driver.close()
