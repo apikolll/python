22fe650d89ad8c245773afb58a19f535b38ba1c9
afiqnexea <afiqnrzm@hotmail.com>
2023-08-13 21:48:26 +0800

    Newlu updated

--- data.py
+++ data.py
@@ -1,13 +1,88 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService 
-CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+from selenium.webdriver.chrome.service import Service as ChromeService
+from bs4 import BeautifulSoup
+import requests
+import cloudscraper
+
+headers = {'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
+
+def yahooFinance(ticker):
+    url = f'https://finance.yahoo.com/quote/{ticker}/key-statistics?p={ticker}'
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    # price = soup.find('fin-streamer', {'data-field': 'regularMarketPrice', 'data-symbol': f'{ticker}'}).text
+
+    # EV1 = soup.find('tr', {'class' : 'Bxz(bb) H(36px) BdB Bdbc($seperatorColor) fi-row Bgc($hoverBgColor):h'})
+    # MC1 = soup.find('tr', {'class': 'Bxz(bb) H(36px) BdY Bdc($seperatorColor) fi-row Bgc($hoverBgColor):h'})
+    # RV1 = soup.find_all('tr', {'class': 'Bxz(bb) H(36px) BdY Bdc($seperatorColor)'})
+    GP1 = soup.find_all('table', {'class': 'W(100%) Bdcl(c)'})
+
+    IS = GP1[7].find_all('tr', {'class': 'Bxz(bb) H(36px) BdB Bdbc($seperatorColor)'})
+
+    # ev = EV1.find('td', {'class' : 'Fw(500) Ta(end) Pstart(10px) Miw(60px)'}).text
+    # mc = MC1.find('td', {'class' : 'Fw(500) Ta(end) Pstart(10px) Miw(60px)'}).text
+    # rv = RV1[6].find('td', {'class': 'Fw(500) Ta(end) Pstart(10px) Miw(60px)'}).text
+    gp = IS[2].find('td', {'class': 'Fw(500) Ta(end) Pstart(10px) Miw(60px)'}).text
+    # ebitda = IS[3].find('td', {'class': 'Fw(500) Ta(end) Pstart(10px) Miw(60px)'}).text
+    # net = IS[4].find('td', {'class': 'Fw(500) Ta(end) Pstart(10px) Miw(60px)'}).text
+
+    # print("*****************************")
+    # print(f'Data for {ticker}')
+    # print(f'Price: {price}')
+    # print(f'EV: {ev}')
+    # print(f'MC: {mc}')
+    # print(f'Revenue: {rv}')
+    # print(f'Gross Profit: {gp}')
+    # print(f'EBITDA: {ebitda}')
+    # print(f'Net Income: {net}')
+    # print("*****************************")
+
+    return gp
+    # print(gp)
 
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")
+def investingData(url):
+    scraper = cloudscraper.CloudScraper()
+    response = scraper.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
 
-driver = webdriver.Chrome(options=options)
+    current = soup.find_all('div', {'class': 'infoLine'})
 
-driver.get("https://www.google.com")
-print(driver.title)
-driver.close()
+    gp_margin = current[0].find('span', {'class', 'float_lang_base_2 text_align_lang_base_2 dirLtr bold'}).text
+    np_margin = current[2].find('span', {'class', 'float_lang_base_2 text_align_lang_base_2 dirLtr bold'}).text
+
+    print(f'Gross Profit Margin: {gp_margin}')
+    print(f'Net Profit Margin: {np_margin}')
+
+def pitchBook(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+
+    stocks = soup.find("div", {'id': 'stock'})
+    financials = soup.find("div", {'id': 'financials'})
+    tbody = financials.find("tbody")
+    tr = tbody.find_all("tr")
+
+    price = soup.find("td", {'class': 'data-table__cell align-right offset-left-XL-5'}).text
+    mc = stocks.find_all("td", {'class': 'data-table__cell align-right pl-xl-10'})
+    ev = tr[0].find_all("td", {'class': 'data-table__cell align-right offset-right-XL-10'})
+    revenue = tr[1].find_all("td", {'class': 'data-table__cell align-right offset-right-XL-10'})
+    ebitda = tr[2].find_all("td", {'class': 'data-table__cell align-right offset-right-XL-10'})
+    netIncome = tr[3].find_all("td", {'class': 'data-table__cell align-right offset-right-XL-10'})
+        
+    results = {
+        "price": price,
+        "marketCap": mc[2].text,
+        "enterpriseValue": ev[0].text,
+        "revenue": revenue[0].text,
+        "ebitda": ebitda[0].text,
+        "netIncome": netIncome[0].text
+    }
+
+    return results
--- ready.py
+++ ready.py
@@ -1,6 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-
+from data import *
 
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -9,12 +9,63 @@ scopes = [
 
 creds = ServiceAccountCredentials.from_json_keyfile_name("./data/creds.json", scopes=scopes)
 
-
 file = gspread.authorize(creds)
 workbook = file.open("python test")
 sheet = workbook.sheet1
 
-cells = sheet.range("A2:A89")
+def changeFormatPitchbook(result):
+    million = "M"
+    billion = "B"
+    trillion = "T"
 
-# for cell in cells:
-#     print(cell.value)
+    if billion in result["marketCap"]:
+        mc = result["marketCap"].split("B")
+        newMC = mc[0].split("$")
+        return float(newMC[1]) * 1000000000
+    elif trillion in  result["marketCap"]:
+        mc = result["marketCap"].split("T")
+        newMC = mc[0].split("$")
+        return float(newMC[1]) * 1000000000000
+    elif million in  result["marketCap"]:
+        mc = result["marketCap"].split("M")
+        newMC = mc[0].split("$")
+        return float(newMC[1]) * 1000000
+    else:
+        return result["marketCap"]
+    
+
+def changeFormatYahoo(result):
+    million = "M"
+    billion = "B"
+    trillion = "T"
+
+    if billion in result:
+        mc = result.split("B")
+        return float(mc[0]) * 1000000000
+    elif trillion in  result:
+        mc = result.split("T")
+        return float(mc[0]) * 1000000000000
+    elif million in  result:
+        mc = result.split("M")
+        return float(mc[0]) * 1000000
+    else:
+        return result
+
+pitchbook = sheet.range("B2:B89")
+yahoo = sheet.range("A2:A89")
+
+for i in range(len(pitchbook)):
+    print(f"Fetching data for {yahoo[i].value}...")
+
+    result_pitchbook = pitchBook(pitchbook[i].value)
+    mc = changeFormatPitchbook(result_pitchbook)
+    # gp = changeFormatYahoo(result2)
+    sheet.update_acell(f"D{i + 2}", f"{result_pitchbook['price']}")
+    sheet.update_acell(f"E{i + 2}", f"{mc}")
+    sheet.update_acell(f"F{i + 2}", f"{result_pitchbook['enterpriseValue']}")
+    sheet.update_acell(f"G{i + 2}", f"{result_pitchbook['revenue']}")
+    sheet.update_acell(f"H{i + 2}", f"{result_pitchbook['ebitda']}")
+    sheet.update_acell(f"I{i + 2}", f"{result_pitchbook['netIncome']}")
+
+
+
